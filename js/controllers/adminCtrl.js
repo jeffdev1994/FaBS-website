eb3b6e0dc9c804b9a8fa4c2ed697fa996b98e5b1
jeffdev1994 <jeffmellis74@gmail.com>
2015-07-09 12:17:50 -0700

    html and moving to adminCTRL finished
    should be all complete now, moved it from main to admin controller. 2
    small TODOs in the html and controller files

--- js/controllers/adminCtrl.js
+++ js/controllers/adminCtrl.js
@@ -36,3 +36,58 @@ angular.module('adminCtrl', ['userService','dataService','ui.bootstrap'])
 	console.log(vm.theuserinfo.username);
 
 })
+
+
+.controller('supportRequest', function(User, Data, $window) {
+	var vm = this;
+
+	vm.requestpromise = User.getRequests();
+	console.log(vm.requestpromise);
+	vm.requests;
+
+	vm.requestpromise.then(function(requests){
+		console.log(requests.data);
+		vm.requests = requests.data;
+	});
+
+	vm.getNiceDate = function(date){
+		return	date.substring(0,10);
+	};
+
+
+
+	vm.openReq = function(body){
+		vex.dialog.alert(body);
+		//vex.dialog.alert('<div style="text-align:center;">Support Request:<br> Problem description here</div>');
+	};
+
+	vm.clearReq = function(requestID){
+		console.log(requestID);
+		vex.dialog.confirm({
+			message: 'Are you sure you want to close this support request?',
+			callback: function(answer) {
+				//if they answer yes
+				if(answer){
+					User.deleteRequest(requestID)
+						.success(function(data){
+							//TODO: this is where the message about them being banned will be displayed
+							if(data.success == false){
+								vex.dialog.alert("Something went wrong! Please contact the farmers market if you wish to find out what happened");
+							}
+							else{
+								vex.dialog.alert('Request is now closed');
+							}
+						});
+
+				}
+				//if they answered no, just dont do anything
+			},
+
+			//refresh page after it is deleted
+			//TODO: it refreshes even if you cancel.  doesnt work if you put the reloud in the callback
+			afterClose: function() {
+				$window.location.reload();
+			}
+		});
+	}
+});
--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -24,58 +24,6 @@ angular.module('mainCtrl', ['userService','dataService','authService','ui.bootst
 	vm.logout = function(){
 		Auth.logout();
 	};
-})
+});
 
-.controller('supportRequest', function(User, Data, Auth,$scope, $window) {
-	var vm = this;
-
-	vm.requestpromise = User.getRequests();
-		console.log(vm.requestpromise);
-	vm.requests;
-
-	vm.requestpromise.then(function(requests){
-		console.log(requests);
-		vm.requests = requests;
-	});
-
-
-
-
-	vm.openReq = function(body){
-		vex.dialog.alert(body);
-     	//vex.dialog.alert('<div style="text-align:center;">Support Request:<br> Problem description here</div>');
-     };
-
-     vm.clearReq = function(requestID){
-     	vex.dialog.alert('Support request cleared!');
-
-
-		 vex.dialog.confirm({
-			 message: 'Are you sure you want to close this support request?',
-			 callback: function(answer) {
-				 //if they answer yes
-				 if(answer){
-					 User.deleteRequest(requestID)
-						 .success(function(data){
-							 //TODO: this is where the message about them being banned will be displayed
-							 if(data.success == false){
-								 vex.dialog.alert( "Something went wrong! Please contact the farmers market if you wish to find out what happened");
-							 }
-							 else{
-								 vex.dialog.alert('Request is now closed');
-							 }
-						 });
-				 }
-				 //if they answered no, just dont do anything
-			 },
-
-			 //refresh page after it is deleted
-			 afterClose: function() {
-				 $window.location.reload();
-			 }
-		 });
-     }
- })
-
-;
 
