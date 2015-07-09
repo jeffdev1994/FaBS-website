448e1871f0ce567b45ce2035d5be562831c83417
Rafat <solar_etc@yahoo.com>
2015-07-09 12:22:58 -0700

    Fixed complete booking functionality. Accounting for sundays. Only thing left is banning users...

--- js/controllers/marketCtrl.js
+++ js/controllers/marketCtrl.js
@@ -22,30 +22,155 @@ angular.module('marketCtrl', ['marketService', 'angularMoment', 'ngDialog'])
             console.log(data, status);
         });
 
-    $scope.deleteBooking2 = function(){
+    var toggleWeekdaySunday = function(day){
+
+        if(day === "Sunday"){
+            // console.log('lol');
+
+            if( $(".weekday").hasClass("hideDiv") === false ){
+                $(".weekday").addClass("hideDiv");
+            }
+            
+            if( $(".sunday").hasClass("hideDiv") === true){
+                $(".sunday").removeClass("hideDiv");
+            }
+        }
+
+        if(day !== "Sunday"){
+            // console.log(day);
+            // console.log("here");
+            if( ($(".weekday").hasClass("hideDiv")) === true ){
+                $(".weekday").removeClass("hideDiv");
+                // console.log("here1");
+
+            }
+
+            if( $(".sunday").hasClass("hideDiv") === false ){
+                $(".sunday").addClass("hideDiv");
+            }
+        }
+    };
+
+    var refreshMap = function(boothList){
+                            
+        for (var j = 0; j < boothList.length; j++){
+
+        var className1 = null;
+        var className2 = null;
+
+        var curDateDay = moment(curDate).format('dddd');
+
+        toggleWeekdaySunday(curDateDay);
+
+        if(curDateDay !== "Sunday"){
+            className1 = "#" + boothList[j] + "-10am";
+            className2 = "#" + boothList[j] + "-4pm";
+        }
+
+        else{
+            // console.log(curDateDay);
+            className1 = "#" + boothList[j] + "-12pm";
+        }
+
+        if ( angular.element($(className1)).hasClass("booth-unavailable") ){
+            angular.element($(className1)).removeClass("booth-unavailable").addClass("booth-available");
+            }
+
+        if(className2 !== null){
+            if( angular.element($(className2)).hasClass("booth-unavailable") ){
+                // console.log(className2);
+                angular.element($(className2)).removeClass("booth-unavailable").addClass("booth-available");
+                }
+            }
+        }
+    };
+
+    //re paint map according to bookings
+    var paintBookings = function(data){
+        if(data.length > 0){
+            for (var i = 0; i < data.length; i++) {
+
+                if (data[i].timeSlot === "12pm") {
+                    angular.element($("#" + data[i].boothName + "-12pm")).removeClass("booth-available").addClass("booth-unavailable");
+                }
+
+                else if (data[i].timeSlot === "10am") {
+                    angular.element($("#" + data[i].boothName + "-10am")).removeClass("booth-available").addClass("booth-unavailable");
+                } else {
+                    // console.log(data[i].boothName);
+                    angular.element($("#" + data[i].boothName + "-4pm")).removeClass("booth-available").addClass("booth-unavailable");
+                }
+            }
+        }
+
+    };
+
+    var refreshScreen = function(curDateDay){
+
+        console.log("Hehehheheheheheheh");
+        console.log(curDate);
+
+        Market.getBookingForDate(curDate)
+            .success(function(data){
+                console.log(data);
+                var boothList = ["Merchandise-1", "Merchandise-2", "Merchandise-3", "Merchandise-4", "Merchandise-5", "Produce-1", "Produce-2", "Produce-3", "Produce-4", "Lunch-1", "Lunch-2", "Lunch-3"];
+                console.log("going into refresh map");
+                refreshMap(boothList);
+                //refresh map to null
+                console.log("data len " + data.length);
+                paintBookings(data);
+                //paint map according to bookings
+
+            })
+
+            .error(function(data, status){
+                alert("Get Booking Failed");
+                console.log(data, status);
+                });
+    };
+
+
+    var deleteBooking2 = function(id){
             
         console.log("lol");
-        Market.cancelBooking($scope.ngDialogData)
+
+        var curDateDay = moment(curDate).format('dddd');
+        Market.cancelBooking(id)
             .success(function(result){
             console.log("Booking has been deleted!");
-            $route.reload();
-            $scope.closeThisDialog();
-            })
+            refreshScreen(curDateDay);
+            console.log($scope.$parent.curDate);
+        })
 
             .error(function(result,status){
             console.log(result,status);
-            $scope.closeThisDialog();
             });
     };
 
     $scope.deleteBooking = function($event){
 
         var id = $event.srcElement.id;
-            var dialog = ngDialog.open({
-            template: "./views/pages/cancelBookingDialog.html",
-            controller: 'marketController',
-            data: id
+
+        vex.dialog.confirm({
+        
+            message: 'Are you sure you want to delete your booking?',
+
+            callback: function(answer) {
+            //if they answer yes, then do the booking
+                if(answer){
+                    deleteBooking2(id);
+                }
+            //if they answered no, just dont do anything
+            },
+
+            //update calander after dialogue closes
+            afterClose: function() {
+                // refreshScreen();
+                $route.reload();
+            }
+
         });
+
     };
 
     $scope.openTimed = function(data) {
@@ -88,31 +213,41 @@ angular.module('marketCtrl', ['marketService', 'angularMoment', 'ngDialog'])
         var modalView;
 
         if(result === true){
-            modalView = "./views/pages/bookedBoothDialog.html";
-            console.log('lol');
+            vex.dialog.alert("This booth is no longer available for booking. Please try booking another booth.");
         }
 
         if(result === false){
-            modalView = "./views/pages/bookingDialog.html";
+        
+        vex.dialog.confirm({
+        
+            message: 'Confirm Booking?',
+
+            callback: function(answer) {
+            //if they answer yes, then do the booking
+                if(answer){
+
+                    confirmBooking(data)
+                }
+            //if they answered no, just dont do anything
+            },
+
+            //update calander after dialogue closes
+            afterClose: function() {
+                refreshScreen();
+            }
+        });
+
         }
 
-        ngDialog.open({
-            template: modalView,
-            className: 'ngdialog-theme-default',
-            controller: 'marketController',
-            data: data
-        });
     };
 
-    $scope.confirmBooking = function(){
-            console.log($scope.ngDialogData);
+    var confirmBooking = function(data){
 
-            Market.saveBooking($scope.ngDialogData)
+            Market.saveBooking(data)
                 .success(function(status){
 
-                    $scope.closeThisDialog();
-                    $scope.openTimed($scope.ngDialogData);
-                    $route.reload();
+                    console.log("Booking confirmed");
+                    $scope.openTimed(data);
                     console.log(status);
                 })
 
@@ -129,100 +264,8 @@ angular.module('marketCtrl', ['marketService', 'angularMoment', 'ngDialog'])
         $scope.Day = moment(curDate).format("dddd, MMMM Do YYYY");
         console.log($scope.Day);
         var curDateDay = moment(curDate).format('dddd');
-
-        Market.getBookingForDate(curDate)
-            .success(function(data){
-                // console.log(data);
-                var boothList = ["Merchandise-1", "Merchandise-2", "Merchandise-3", "Merchandise-4", "Merchandise-5", "Produce-1", "Produce-2", "Produce-3", "Produce-4", "Lunch-1", "Lunch-2", "Lunch-3"];
-                
-                var toggleWeekdaySunday = function(day){
-
-                    if(day === "Sunday"){
-                        // console.log('lol');
-
-                        if( $(".weekday").hasClass("hideDiv") === false ){
-                            $(".weekday").addClass("hideDiv");
-                        }
-                        
-                        if( $(".sunday").hasClass("hideDiv") === true){
-                            $(".sunday").removeClass("hideDiv");
-                        }
-                    }
-
-                    if(day !== "Sunday"){
-                        // console.log(day);
-                        // console.log("here");
-                        if( ($(".weekday").hasClass("hideDiv")) === true ){
-                            $(".weekday").removeClass("hideDiv");
-                            // console.log("here1");
-
-                        }
-
-                        if( $(".sunday").hasClass("hideDiv") === false ){
-                            $(".sunday").addClass("hideDiv");
-                        }
-                    }
-                };
-
-                var refreshMap = function(){
-                    
-                    for (var j = 0; j < boothList.length; j++){
-
-                    var className1 = null;
-                    var className2 = null;
-
-                    toggleWeekdaySunday(curDateDay);
-
-                    if(curDateDay !== "Sunday"){
-                        className1 = "#" + boothList[j] + "-10am";
-                        className2 = "#" + boothList[j] + "-4pm";
-                    }
-
-                    else{
-                        className1 = "#" + boothList[j] + "-12pm";
-                    }
-
-                    if ( angular.element($(className1).hasClass("booth-unavailable")) ){
-                        // console.log(className1);
-                        angular.element($(className1)).removeClass("booth-unavailable").addClass("booth-available");
-                        }
-
-                    if(className2 !== null){
-                        if( angular.element($(className2).hasClass("booth-unavailable")) ){
-                            // console.log(className2);
-                            angular.element($(className2)).removeClass("booth-unavailable").addClass("booth-available");
-                            }
-                        }
-                    }
-                };
-
-                //re paint map according to bookings
-               var paintBookings = function(data){
-                    if(data.length > 0){
-                        for (var i = 0; i < data.length; i++) {
-                            if (data[i].timeSlot === "10am") {
-                                angular.element($("#" + data[i].boothName + "-10am")).removeClass("booth-available").addClass("booth-unavailable");
-                            } else {
-                                // console.log(data[i].boothName);
-                                angular.element($("#" + data[i].boothName + "-4pm")).removeClass("booth-available").addClass("booth-unavailable");
-                            }
-                        }
-                    }
-
-                };
-
-                refreshMap();
-                //refresh map to null
-                console.log("data len " + data.length);
-
-                paintBookings(data);
-                //paint map according to bookings
-
-            })
-
-        .error(function(data, status){
-            alert("Get Booking Failed");
-            console.log(data, status);
-        });
+        refreshScreen(curDateDay);
     });
+
+
 });
